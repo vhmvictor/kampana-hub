fa60a2b32122b7126f245e2ff1603f6a1aee37b7
Victor Hugo <victor.hugomarques@hotmail.com>
2021-03-30 15:30:10 -0300

    update

--- database/index.js
+++ database/index.js
@@ -3,7 +3,8 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const uri = "mongodb://" + process.env.DB_USER + ":" + process.env.DB_PASS + "@kampana-shard-00-00.0ppb7.mongodb.net:27017,kampana-shard-00-01.0ppb7.mongodb.net:27017,kampana-shard-00-02.0ppb7.mongodb.net:27017/" + process.env.DB_NAME +"?ssl=true&replicaSet=atlas-96z7k6-shard-0&authSource=admin&retryWrites=true&w=majority";
+//const uri = "mongodb://" + process.env.DB_USER + ":" + process.env.DB_PASS + "@kampana-shard-00-00.0ppb7.mongodb.net:27017,kampana-shard-00-01.0ppb7.mongodb.net:27017,kampana-shard-00-02.0ppb7.mongodb.net:27017/" + process.env.DB_NAME +"?ssl=true&replicaSet=atlas-96z7k6-shard-0&authSource=admin&retryWrites=true&w=majority";
+const uri = "mongodb+srv://kampana_digital:Kampana2021@kampana.0ppb7.mongodb.net/kampana_hub?retryWrites=true&w=majority"
 
 mongoose.connect(uri, {
     useUnifiedTopology: true,
